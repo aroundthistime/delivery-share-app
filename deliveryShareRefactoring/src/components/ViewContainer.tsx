f08abd5e39e27fdb1731e7ccb86b082750529ae2
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2022-01-12 17:18:30 +0900

    리팩토링 시작

---
+++ deliveryShareRefactoring/src/components/ViewContainer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+export default styled.View`
+    justify-content : center;
+    align-items : center;
+    flex : 1;
+    background-color : white;
+`
